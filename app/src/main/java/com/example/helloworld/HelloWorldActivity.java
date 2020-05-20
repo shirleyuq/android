980a5049575a132a4ebe8b9c033f0dacbc00c205
shirley <liuchun_0206@163.com>
2020-05-20 10:50:40 +1000

    first commit

---
+++ app/src/main/java/com/example/helloworld/HelloWorldActivity.java
@@ -0,0 +1,16 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class HelloWorldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_layout);
+        Log.d("data","onCreate execute");
+    }
+}
